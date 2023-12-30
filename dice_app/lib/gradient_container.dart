df861457933429a35d064f0cf4447008adc6bd5a
Prajjawal <prajjawaldy.zhcet@gmail.com>
2023-12-30 15:42:56 +0530

    first commit and added variables

---
+++ dice_app/lib/gradient_container.dart
@@ -0,0 +1,24 @@
+import 'package:dice_app/styled_text.dart';
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 3, 108, 98),
+            Color.fromARGB(255, 8, 187, 169)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
---
+++ dice_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:dice_app/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
